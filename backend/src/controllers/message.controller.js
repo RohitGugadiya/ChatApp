cf12066f325a6bbabb432559e26044019c339f43
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-01-31 08:49:36 +1100

    Message route setup and getting ready with frontend

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,60 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filterdUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filterdUsers);
+  } catch (error) {
+    console.log("error in update profile", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.prams;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { receiverId: userToChatId, senderId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("error in update profile", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.prams;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.saave();
+  } catch (error) {
+    console.log("error in update profile", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/auth.route.js";
+
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -9,8 +12,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
