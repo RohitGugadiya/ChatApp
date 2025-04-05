a1e0650e3e4a2cd89f99f8f43e97e8bf528e4b24
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-04-05 14:36:35 +1100

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,14 +13,14 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5005;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://buzztalk.site",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
