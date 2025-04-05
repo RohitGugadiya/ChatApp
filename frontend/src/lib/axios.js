cf2e972e0df6fa3c503e4bd61f3a18afb1c04611
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-04-05 11:52:41 +1100

    Env Change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "http://buzztalk.site:5001/api",
+      : "/api",
   withCredentials: true,
 });
