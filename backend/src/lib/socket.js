feb84f8c6d2fa8f36a9237fec79a092fbc55b851
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-04-14 23:35:27 +0930

    VPS fix

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://buzztalk.site"], //["http://localhost:5173"],
+    origin: ["http://buzztalk.site"], //["http://localhost:5173"],
     credentials: true,
   },
 });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://buzztalk.site:5001/api", //import.meta.env.VITE_BACKEND_URL,
+  baseURL: "http://buzztalk.site:5001/api", //import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
