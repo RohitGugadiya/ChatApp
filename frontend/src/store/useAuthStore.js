42dab332909ee869961911b5a2f8676678ec3686
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-04-14 19:42:19 +0930

    VPS setup

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,10 +3,10 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-// const BASE_URL =
-//   import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; //back
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; //back
 
-const BASE_URL = import.meta.env.VITE_SOCKET_URL;
+// const BASE_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
