a220a550e96d7ef7f9b2e4dc65406feb4bd64190
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-10-08 22:08:11 +1030

    Cors update

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "None",
-    secure: true, //process.env.NODE_ENV === "production",
+    // secure: true, //process.env.NODE_ENV === "production",
   });
 
   return token;
