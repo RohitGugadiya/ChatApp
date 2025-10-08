384b5db45fdf1ee4173e134d00fa941029128edf
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-10-08 21:19:27 +1030

    CORS change

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://buzztalk.site",
+    origin: "http://www.buzztalk.site",
     credentials: true,
   })
 );
