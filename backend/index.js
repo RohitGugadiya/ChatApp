045644cff29ab73dc8bbab62fbea2e9d68b34014
RohitGugadiya <rohitgugadiya3@gmail.com>
2025-10-08 18:40:12 +1030

    Production change

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,14 @@ import { app, server } from "./src/lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://buzztalk.site",
     credentials: true,
   })
 );
@@ -28,13 +28,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "https://localhost:5001" : "/"; //back
+const BASE_URL = import.meta.env.VITE_BACKEND_URL; // "http://localhost:5001/api";
+// import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/"; //back
 
 // const BASE_URL = import.meta.env.VITE_SOCKET_URL;
 
